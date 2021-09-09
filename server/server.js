99fc5e6d6ffe42643db86aa560909cefb8e9befb
abhi-13-07 <abhishek.13.july.02@gmail.com>
2021-09-09 12:19:41 +0530

    connect to Database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
+
+(async () => {
+	try {
+		const { connection } = await mongoose.connect(process.env.DATABASE_URI);
+		console.log(`Connected to Database(${connection.name})`);
+	} catch (err) {
+		console.log('error while connecting to db', err.message);
+	}
+})();
 
 app.listen(process.env.PORT, () =>
 	console.log(`Server started on port ${process.env.PORT}`)
