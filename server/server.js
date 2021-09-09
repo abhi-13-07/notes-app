606473b30055e13f82e17d8708d7f43fec8315fe
abhi-13-07 <abhishek.13.july.02@gmail.com>
2021-09-09 12:02:33 +0530

    initial server setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () =>
+	console.log(`Server started on port ${process.env.PORT}`)
+);
